45210eb6c9cef81becdca752ee6dc9fbdb883373
andreikurnosov <nixdie@yandex.ru>
2021-02-09 16:08:33 +0700

    fetch game detail

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import styled from 'styled-components'
 import { motion } from 'framer-motion'
+import { useDispatch } from 'react-redux'
+import { loadDetail } from './../actions/detailAction'
 
 const StyledGame = styled(motion.div)`
   min-height: 30vh;
   box-shadow: 0px 5px 20px rgba(0, 0, 0, 0.2);
   text-align: center;
   border-radius: 1rem;
+  cursor: pointer;
   img {
     width: 100%;
     height: 40vh;
@@ -14,9 +17,13 @@ const StyledGame = styled(motion.div)`
   }
 `
 
-function Game({ name, released, image }) {
+function Game({ name, released, image, id }) {
+  const dispatch = useDispatch()
+  const loadDetailHandler = () => {
+    dispatch(loadDetail(id))
+  }
   return (
-    <StyledGame>
+    <StyledGame onClick={loadDetailHandler}>
       <h3>{name}</h3>
       <p>{released}</p>
       <img src={image} alt={name} />
